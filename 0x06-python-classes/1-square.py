c5562287b318f6e61bd64e9565e3a1a383467567
Whisper Edia <ediaadada@gmail.com>
2022-06-22 02:22:35 +0100

    square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+# 1-square.py
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size#!/usr/bin/python3
 class Square:
     def __init__(self, size):
         self.__size = size
